7582bc206a8c34fbc2ce15f478e0515401da23e9
Brett Tasker <brett@silverstripe.com>
2022-08-12 15:05:40 +1200

    Convert plugin options to flags

--- manifests/config.pp
+++ manifests/config.pp
@@ -8,13 +8,21 @@ class ss_osquery::config inherits ::ss_osquery {
         content => template('ss_osquery/osquery.conf.erb'),
         require => Package['osquery'],
     }
+    -> file { '/etc/osquery/osquery.flags':
+        ensure  => 'file',
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
+        content => template('ss_osquery/osquery.flags.erb'),
+        require => Package['osquery'],
+    }
     -> service { 'osqueryd':
         ensure     => true,
         enable     => true,
         hasstatus  => true,
         hasrestart => true,
         require    => Package['osquery'],
-        subscribe  => File['/etc/osquery/osquery.conf'],
+        subscribe  => [File['/etc/osquery/osquery.conf'], File['/etc/osquery/osquery.flags']],
     }
 
     file { '/etc/rsyslog.d/osquery.conf':
